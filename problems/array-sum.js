5af92cc7444a64e06f3524be300f0547a33edc2a
Lorin Steel <lorinsteel@gmail.com>
2015-09-28 23:12:07 -0600

    I dun ficksed 'et.

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function arraySum(arr) {
-
+  var total = 0;
+  arrayArraySum(arr);
+  function arrayArraySum(arr) {
+    Object.keys(arr).forEach(function(key) {
+      (arr[key] === parseInt(arr[key]) ? total+=arr[key] : arrayArraySum(arr[key]) );
+    });
+  }
+  return total;
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function longestString(arr) {
-
+  var longString = '';
+  Object.keys(arr).forEach(function(key) {
+    ((typeof arr[key]) === 'string' ? stringLength(arr[key]) : longString = longString );
+  });
+  function stringLength(string) {
+    (string.length > longString.length ? longString = string : longString = longString );
+  }
+  return longString;
 }
 
 module.exports = longestString;
